46e0385a19db84ff2d5eb8710e6d3190e8d6e52d
Christian Sagel <c.sagel@digipen.edu>
2016-11-21 23:07:01 -0800

    Not much!

---
+++ SHPlanner/Assets/Door.cs
@@ -0,0 +1,18 @@
+/******************************************************************************/
+/*!
+@file   Door.cs
+@author Christian Sagel
+@par    email: c.sagel\@digipen.edu
+@par    DigiPen login: c.sagel
+*/
+/******************************************************************************/
+using UnityEngine;
+using Stratus;
+
+namespace SHPlanner 
+{
+  public class Door : StratusBehaviour 
+  {
+  
+  }  
+}
---
+++ SHPlanner/Assets/SmartObject.cs
@@ -0,0 +1,19 @@
+/******************************************************************************/
+/*!
+@file   SmartObject.cs
+@author Christian Sagel
+@par    email: c.sagel\@digipen.edu
+@par    DigiPen login: c.sagel
+*/
+/******************************************************************************/
+using UnityEngine;
+using Stratus;
+
+namespace SHPlanner 
+{
+  public abstract class SmartObject : StratusBehaviour 
+  {
+        
+      
+  }  
+}
