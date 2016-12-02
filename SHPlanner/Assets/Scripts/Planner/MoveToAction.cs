fcd12334a6a3fb144765089422fb740583f985e0
Christian Sagel <c.sagel@digipen.edu>
2016-12-02 08:02:05 -0800

    no message

---
+++ SHPlanner/Assets/Scripts/Planner/MoveToAction.cs
@@ -0,0 +1,37 @@
+/******************************************************************************/
+/*!
+@file   MoveToAction.cs
+@author Christian Sagel
+@par    email: c.sagel\@digipen.edu
+@par    DigiPen login: c.sagel
+*/
+/******************************************************************************/
+using UnityEngine;
+using Stratus;
+using System;
+
+namespace Prototype 
+{
+  public class MoveToAction : Action
+  {
+    protected override void OnStart()
+    {
+      
+    }
+
+    protected override void OnEnd()
+    {
+      
+    }
+
+    protected override void OnExecute()
+    {
+      
+    }
+
+    protected override bool OnValidate()
+    {
+      return false;
+    }
+  }
+}
